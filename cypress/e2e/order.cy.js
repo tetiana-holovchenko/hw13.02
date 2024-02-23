1faf0b2b011ee1048599ae43674887f94d2d61f9
tetiana-holovchenko <tetiana.holovchenko@planhat.com>
2024-02-23 16:12:43 +0200

    adding files

---
+++ cypress/e2e/order.cy.js
@@ -0,0 +1,13 @@
+
+
+
+import { findItem } from '../support/helper';
+
+describe('item search', () => {
+  it('search', () => {
+    cy.visit('https://automationteststore.com/');
+    cy.get('#filter_keyword').type('E');
+    cy.get('.button-in-search').click();
+    findItem('Fiorella Purple Peep Toes'); 
+});
+});
